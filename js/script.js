dbe9196b1c770d256e703d5049fde33563f30d89
Oma Withers <omacwitherssei@gmail.com>
2022-08-22 13:14:44 -0400

    Corrected API and Api hit is now successfull

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
-const axios = require("axios");
 
-const options = {
-  method: 'POST',
-  url: 'https://motivational-quotes1.p.rapidapi.com/motivation',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': '2c7bc1880bmshbc56370d627469cp1aef62jsn3ccfa31b1ad3',
-    'X-RapidAPI-Host': 'motivational-quotes1.p.rapidapi.com'
-  },
-  data: '{"key1":"value","key2":"value"}'
+
+
+// API Begins Here 
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://motivational-quotes1.p.rapidapi.com/motivation",
+	"method": "POST",
+	"headers": {
+		"content-type": "application/json",
+		"X-RapidAPI-Key": "2c7bc1880bmshbc56370d627469cp1aef62jsn3ccfa31b1ad3",
+		"X-RapidAPI-Host": "motivational-quotes1.p.rapidapi.com"
+	},
+	"processData": false,
+	"data": {
+		"key1": "value",
+		"key2": "value"
+	}
 };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
+$.ajax(settings).done(function (response) {
+	console.log(response);
 });
