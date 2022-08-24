3de6f63b708c57c38ee90e4a2d2ab628b9b9c557
Oma Withers <omacwitherssei@gmail.com>
2022-08-24 12:14:00 -0400

    git commit -m "correction" 	modified:   css/style.css 	modified:   index.html 	modified:   js/script.js

--- js/script.js
+++ js/script.js
@@ -2,23 +2,31 @@
 
 
 // API Begins Here 
-const settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://motivational-quotes1.p.rapidapi.com/motivation",
-	"method": "POST",
-	"headers": {
-		"content-type": "application/json",
-		"X-RapidAPI-Key": "2c7bc1880bmshbc56370d627469cp1aef62jsn3ccfa31b1ad3",
-		"X-RapidAPI-Host": "motivational-quotes1.p.rapidapi.com"
-	},
-	"processData": false,
-	"data": {
-		"key1": "value",
-		"key2": "value"
-	}
+const api = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://motivational-quotes1.p.rapidapi.com/motivation",
+    "method": "POST",
+    "headers": {
+        "content-type": "application/json",
+        "X-RapidAPI-Key": "2c7bc1880bmshbc56370d627469cp1aef62jsn3ccfa31b1ad3",
+        "X-RapidAPI-Host": "motivational-quotes1.p.rapidapi.com"
+    },
+    "processData": false,
+    "data": {
+        "key1": "value",
+        "key2": "value"
+    }
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
+$.ajax(api).done(function (response) {
+    console.log(response);
+
+const button1= document.getElementById('quote').innerHTML = response;
+
 });
+
+
+
+
+
